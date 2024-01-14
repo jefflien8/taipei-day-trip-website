326d88c61a2efe675c3864309891b62bc16dd9c5
jefflien8 <jefflien8@gmail.com>
2024-01-15 05:25:50 +0800

    session

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 from api import apiBlueprint
+from datetime import timedelta
 
 app=Flask(__name__,static_folder="public", static_url_path="/public")
 app.register_blueprint(apiBlueprint)
@@ -10,6 +11,12 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 
 
 # Pages
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
+    if not session.get('name'):
+        session['name'] = None
 @app.route("/")
 def index():
 	return render_template("index.html")
@@ -22,5 +29,6 @@ def booking():
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
+
 
 app.run(host='0.0.0.0',port=3000)
